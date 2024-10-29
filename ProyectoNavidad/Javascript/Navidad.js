bf6c09dcc82e7bd424acaeed6644a1133842d00e
unknown <alumnofp@FPB16.CALASANZSALAMANCA.local>
2024-10-29 10:12:01 +0100

    enlaces para mañana

---
+++ ProyectoNavidad/Javascript/Navidad.js
@@ -0,0 +1,9 @@
+function enlaces(){
+
+document.getElementById('enlaces').addEventListener('change', function() {
+    var selectedValue = this.value;
+    if (selectedValue) {
+        window.location.href = selectedValue; // Redirige a la URL seleccionada
+    }
+});
+}enlaces();
