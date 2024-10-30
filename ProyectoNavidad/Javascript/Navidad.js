3447da4105b6d6370fe07d6eee531a5a51b2f24f
unknown <alumnofp@FPB16.CALASANZSALAMANCA.local>
2024-10-30 10:56:51 +0100

    ewewe

--- ProyectoNavidad/Javascript/Navidad.js
+++ ProyectoNavidad/Javascript/Navidad.js
@@ -1,9 +1,7 @@
-function enlaces(){
-
 document.getElementById('enlaces').addEventListener('change', function() {
     var selectedValue = this.value;
     if (selectedValue) {
         window.location.href = selectedValue; // Redirige a la URL seleccionada
     }
 });
-}enlaces();
+
